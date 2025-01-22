f8a46553942d8d8fa4f694ef5bc83eaef703b7ac
lucio <lucio.ese@gmail.com>
2025-01-22 16:28:42 -0300

    feat: Show username placeholder and timestamp in messages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const http = require('http')
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,21 +11,23 @@ const io = socketio(server);
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'BOT'
+
 // run when client connects
 io.on('connection', (socket) => {
     // new client
-    socket.emit('message', 'Welcome to Realtime Chat!'); // esto emite un evento con nombre "message" y un arg // esto va para un solo cliente
+    socket.emit('message', formatMessage(botName,'Welcome to Realtime Chat!')); // esto emite un evento con nombre "message" y un arg // esto va para un solo cliente
 
-    socket.broadcast.emit('message', 'A user has joined the chat'); // el broadcast significa que lo va a emitir a todos los clientes MENOS al que "realice" la acción
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat')); // el broadcast significa que lo va a emitir a todos los clientes MENOS al que "realice" la acción
 
     // when client disconnect
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message', formatMessage(botName,'A user has left the chat'))
     })
 
     // listen for chat chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     })
 })
 
